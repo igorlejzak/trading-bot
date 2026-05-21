542418439596f571d7d1af6938f0bc3a27cf624f
igorlejzak <pinga3009@gmail.com>
2026-05-21 23:57:59 +0200

    add entry signals for long and short

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from ta.volatility import BollingerBands
 from ta.momentum import StochRSIIndicator 
 
 
-
 exchange = ccxt.bybit()
 swieczki = exchange.fetch_ohlcv('BTC/USDT', timeframe='15m', limit=100)
 df = pd.DataFrame(swieczki, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
@@ -19,9 +18,22 @@ df['bb_bbm'] = indicator_bb.bollinger_mavg()
 df['bb_bbh'] = indicator_bb.bollinger_hband()
 df['bb_bbl'] = indicator_bb.bollinger_lband()
 
-df['bb_bbhi'] = indicator_bb.bollinger_hband_indicator()
-df['bb_bbli'] = indicator_bb.bollinger_lband_indicator()
+
 df['stochrsi_k'] = indicator_stochrsi.stochrsi_k()
 df['stochrsi_d'] = indicator_stochrsi.stochrsi_d()
+
+df['stochrsi_k'] = df['stochrsi_k'] * 100
+df['stochrsi_d'] = df['stochrsi_d'] * 100
+
+ostatnia = df.iloc[-1]
+
+if ostatnia['close'] > ostatnia['bb_bbh'] * 1.01 and ostatnia['stochrsi_k'] > 80 and ostatnia['stochrsi_d'] > 80:
+    print("SYGNAŁ SHORT")
+
+elif ostatnia['close'] < ostatnia['bb_bbl'] * 0.99 and ostatnia['stochrsi_k'] < 20 and ostatnia['stochrsi_d'] < 20:
+    print("SYGNAŁ LONG")
+
+else:
+    print("brak sygnału")
 
 print(df)
