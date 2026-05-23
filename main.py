4371a05114d45043a452ab396647924d016d1588
igorlejzak <pinga3009@gmail.com>
2026-05-23 15:16:16 +0200

    multicoin monitoring for 5 pairs

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import time
 from ta.volatility import BollingerBands
 from ta.momentum import StochRSIIndicator 
 
-def pobierz_dane():
+def pobierz_dane(symbol):
     
-    swieczki = exchange.fetch_ohlcv('BTC/USDT', timeframe='15m', limit=100)
+    swieczki = exchange.fetch_ohlcv(symbol, timeframe='15m', limit=100)
     df = pd.DataFrame(swieczki, columns=['timestamp','open','high','low','close','volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
     indicator_bb = BollingerBands(close=df["close"], window=20, window_dev=2)
@@ -30,45 +30,57 @@ def pobierz_dane():
 exchange = ccxt.bybit()
 pozycja = {"otwarta": False, "typ": None, "entry": None}
 
+coiny = ["BTC/USDT", "ETH/USDT", "SOL/USDT", "XRP/USDT", "HYPE/USDT"]
+pozycje = {
+    "BTC/USDT": {"otwarta": False, "typ": None, "entry": None},
+    "ETH/USDT": {"otwarta": False, "typ": None, "entry": None},
+    "SOL/USDT": {"otwarta": False, "typ": None, "entry": None},
+    "XRP/USDT": {"otwarta": False, "typ": None, "entry": None},
+    "HYPE/USDT": {"otwarta": False, "typ": None, "entry": None}
+}
+
 while True:
-    df = pobierz_dane()
-    cena = df['close'].iloc[-1]   # aktualna cena (ostatnia świeczka)
-    ostatnia = df.iloc[-1]
+    for coin in coiny:
+        df = pobierz_dane(coin)           # pobierz dane TEGO coina
+        pozycja = pozycje[coin]
 
-    if not pozycja["otwarta"]:
-        # NIE MAM POZYCJI → szukam sygnału wejścia
-        if cena > ostatnia['bb_bbh'] * 1.01 and ostatnia['stochrsi_k'] > 80 and ostatnia['stochrsi_d'] > 80:
-            pozycja["otwarta"] = True
-            pozycja["typ"] = "short"
-            pozycja["entry"] = cena
-            print(f"OTWARTO SHORT po cenie {cena}")
+        ostatnia = df.iloc[-1]
+        cena = df['close'].iloc[-1]
 
-        elif cena < ostatnia['bb_bbl'] * 0.99 and ostatnia['stochrsi_k'] < 20 and ostatnia['stochrsi_d'] < 20:
-            pozycja["otwarta"] = True
-            pozycja["typ"] = "long"
-            pozycja["entry"] = cena
-            print(f"OTWARTO LONG po cenie {cena}")
+        if not pozycja["otwarta"]:
+            # NIE MAM POZYCJI → szukam sygnału wejścia
+            if cena > ostatnia['bb_bbh'] * 1.01 and ostatnia['stochrsi_k'] > 80 and ostatnia['stochrsi_d'] > 80:
+                pozycja["otwarta"] = True
+                pozycja["typ"] = "short"
+                pozycja["entry"] = cena
+                print(f"OTWARTO SHORT po cenie {cena}")
 
-    else:   # MAM POZYCJĘ → szukam sygnału wyjścia
-        entry= pozycja["entry"]
-        if pozycja["typ"] == "short":
-            tp = entry * 0.99
-            sl = entry * 1.03
-            if cena <= tp:
-                pozycja["otwarta"] = False
-                print(f"ZAMKNIĘTO SHORT z zyskiem po cenie {cena}")
-            if cena >= sl:
-                pozycja["otwarta"] = False
-                print(f"ZAMKNIĘTO SHORT ze stratą po cenie {cena}")
+            elif cena < ostatnia['bb_bbl'] * 0.99 and ostatnia['stochrsi_k'] < 20 and ostatnia['stochrsi_d'] < 20:
+                pozycja["otwarta"] = True
+                pozycja["typ"] = "long"
+                pozycja["entry"] = cena
+                print(f"OTWARTO LONG po cenie {cena}")
 
-        elif pozycja["typ"] == "long":
-            tp = entry * 1.01
-            sl = entry * 0.97
-            if cena >= tp:
-                pozycja["otwarta"] = False
-                print(f"ZAMKNIĘTO LONG z zyskiem po cenie {cena}")
-            if cena <= sl:
-                pozycja["otwarta"] = False
-                print(f"ZAMKNIĘTO LONG ze stratą po cenie {cena}")
-    print(f"Sprawdzam... cena: {cena}, BB dolna: {ostatnia['bb_bbl']:.1f}, StochRSI: {ostatnia['stochrsi_k']:.1f}")
-    time.sleep(60)
+        else:   # MAM POZYCJĘ → szukam sygnału wyjścia
+            entry= pozycja["entry"]
+            if pozycja["typ"] == "short":
+                tp = entry * 0.99
+                sl = entry * 1.03
+                if cena <= tp:
+                    pozycja["otwarta"] = False
+                    print(f"ZAMKNIĘTO SHORT z zyskiem po cenie {cena}")
+                if cena >= sl:
+                    pozycja["otwarta"] = False
+                    print(f"ZAMKNIĘTO SHORT ze stratą po cenie {cena}")
+
+            elif pozycja["typ"] == "long":
+                tp = entry * 1.01
+                sl = entry * 0.97
+                if cena >= tp:
+                    pozycja["otwarta"] = False
+                    print(f"ZAMKNIĘTO LONG z zyskiem po cenie {cena}")
+                if cena <= sl:
+                    pozycja["otwarta"] = False
+                    print(f"ZAMKNIĘTO LONG ze stratą po cenie {cena}")
+        print(f"Sprawdzam... cena: {cena}, BB dolna: {ostatnia['bb_bbl']:.1f}, StochRSI: {ostatnia['stochrsi_k']:.1f}")
+    time.sleep(5)
